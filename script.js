1e2d1e70a09df61c058be77926dbeb56bdd56509
robin12 <giocardona2394@gmail.com>
2024-11-14 17:17:02 -0500

    control de versiones

---
+++ script.js
@@ -0,0 +1,12 @@
+//const modal=document.getElementById("modal");
+
+//function openModal(){
+    ///modal.style.display=flex
+//}
+
+function openModal(){
+    document.getElementsById("modal").style.display="flex";
+} 
+function closeModal(){
+    document.getElementsById("modal").style.display="none";
+}
